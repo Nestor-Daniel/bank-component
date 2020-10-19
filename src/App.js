a263dc301b9d8a1a69f8b727f8785dd29b4a1c04
Nestor <daniel16ac@gmail.com>
2020-10-18 19:35:54 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default function App(){
   const [isSwift, setSwift] = useState(true);
   const [isEUCountry, setEUCountry] = useState(false);
   const [currency, setCurrency] = useState();
+  const [country, setCountry] = useState();
 
   const openModal = () => setShow(true);
   const closeModal = () => setShow(false);
@@ -26,9 +27,23 @@ export default function App(){
         case 'GER':
           setEUCountry(false);
           setCurrency('EUR');
+          setCountry('GER')
           break;
         case 'UK':
           setEUCountry(true);
+          setCurrency('GBP');
+          setCountry('UK')
+          break;
+        default:
+          break;
+      }
+  }
+  const selectCurrency = (props) => {
+      switch (props.target.value) {
+        case 'EUR':
+          setCurrency('EUR');
+          break;
+        case 'GBP':
           setCurrency('GBP');
           break;
         default:
@@ -56,29 +71,34 @@ export default function App(){
                 <Form.Text>
                   Once you submit you bank information, we'll verify
                   you information before we start sending any payout there.
-                </Form.Text>
+                </Form.Text><br/>
+                {country==='GER'?<Form.Label>When will I get paid?</Form.Label>:null}
+                {country==='GER'?<Form.Text>
+                  We need to collect this Information to send
+                  payments to you on your employer's behalf.
+                </Form.Text>:null}
                 <div className='spaceFill'></div>
               </Form.Group>
-              <Form.Group as={Col}className='flexComp'>
+              <Form.Group as={Col} className='flexComp'>
                 {isSwift?<Form.Label>Bank Country</Form.Label>:null}
-                {isSwift?<Form.Control as="select" defaultValue="" onChange = {selectCountry}>
+                {isSwift?<Form.Control as="select" value = {country} onChange = {selectCountry}>
                   <option value = 'GER' >Germany</option>
                   <option value = 'UK' >United Kingdom</option>
-                </Form.Control>:null}
+                </Form.Control>:null}<br/>
                 <Form.Label>Select Method</Form.Label><br/>
                 <ToggleButtonGroup type="radio" name="options" defaultValue='SWIFT' onChange={setAccount}>
                   <ToggleButton value='SWIFT' variant="success" >SWIFT</ToggleButton>
                   <ToggleButton value='IBAN' variant="success" >IBAN</ToggleButton>
                 </ToggleButtonGroup><br/>
                 <div className='spaceFill'></div>
-                <Form.Label>SWIFT</Form.Label>
+                <Form.Label>{isSwift?'SWIFT':'IBAN'}</Form.Label>
                 <Form.Control />
                 {isSwift?<Form.Label>Account Number</Form.Label>:null}
                 {isSwift?<Form.Control />:null}
               </Form.Group>
               <Form.Group as={Col} className='flexComp'>
                 {isSwift?<Form.Label>Currency</Form.Label>:null}
-                {isSwift?<Form.Control as="select" value = {currency}>
+                {isSwift?<Form.Control as="select" value = {currency} onChange = {selectCurrency}>
                   <option>EUR</option>
                   <option>GBP</option>
                 </Form.Control>:null}
@@ -86,7 +106,7 @@ export default function App(){
                 {isSwift && isEUCountry?<Form.Label>Rounting Number</Form.Label>:null}
                 {isSwift && isEUCountry?<Form.Control/>:null}
                 {isSwift?<Form.Label>Confirm Account Number</Form.Label>:<Form.Label>Currency</Form.Label>}
-                {isSwift?<Form.Control/>:<Form.Control as="select" value = {currency}>
+                {isSwift?<Form.Control/>:<Form.Control as="select" value = {currency} onChange = {selectCurrency}>
                   <option>EUR</option>
                   <option>GBP</option>
                 </Form.Control>}
